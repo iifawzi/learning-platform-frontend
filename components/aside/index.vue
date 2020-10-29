1e9e10efb6edd40964952955aea88780ff507398
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-10-29 11:59:49 +0200

    add aside's content

--- components/aside/index.vue
+++ components/aside/index.vue
@@ -1,14 +1,52 @@
 <template>
-    <aside class="dashboard-aside">
-    </aside>
+  <aside class="dashboard-aside">
+    <div class="aside__content">
+      <div class="aside__content--top">LOGO</div>
+      <div class="aside__content--bottom">
+        <ul class="bottom__list">
+          <li>
+            <nuxt-link to="/dashboard" exact class="list-item-link">
+              <span class="list-item__icon">
+                <i class="fas fa-columns"></i>
+              </span>
+              <span class="list-item__name"> Dashboard </span>
+            </nuxt-link>
+          </li>
+           <li>
+            <nuxt-link to="/groups" class="list-item-link">
+              <span class="list-item__icon">
+                <i class="fas fa-users"></i>
+              </span>
+              <span class="list-item__name"> Groups </span>
+            </nuxt-link>
+          </li>
+           <li>
+            <nuxt-link to="/rooms" class="list-item-link">
+              <span class="list-item__icon">
+               <i class="fas fa-print"></i>
+              </span>
+              <span class="list-item__name"> Exams </span>
+            </nuxt-link>
+          </li>
+           <li>
+            <nuxt-link to="/sections" class="list-item-link">
+              <span class="list-item__icon">
+               <i class="fas fa-comments"></i>
+              </span>
+              <span class="list-item__name"> Chat </span>
+            </nuxt-link>
+          </li>
+        </ul>
+        <img class="bottom__icon" src="~/assets/images/aside/books.png"/>
+      </div>
+    </div>
+  </aside>
 </template>
 
 
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="scss" scoped src="./style.scss" />
