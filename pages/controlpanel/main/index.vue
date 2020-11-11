175def9c2871dec1e2dde60ad7b973fef06fe750
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-11-11 21:03:57 +0200

    delete pageInfo

--- pages/controlpanel/main/index.vue
+++ pages/controlpanel/main/index.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="cp">
       <div class="cp__content">
-        <pageInfo title="لوحة التحكم"/>
+       
       </div>
     </div>
 </template>
 
 
 <script>
-import pageInfo from "~/components/shared/pageInfo"
 export default {
   components: {
     pageInfo,
